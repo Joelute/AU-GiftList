535bbd60520ef9b111077d6cae535690b4cbc60d
Joseph Ho <josephho678@gmail.com>
2025-02-02 15:36:08 -0500

    verify: Verify users that are on the list.

--- client/index.js
+++ client/index.js
@@ -6,9 +6,17 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const LIST_LENGTH = niceList.length;
+  const merkleTree = new MerkleTree(niceList);
+
+  const INDEX = Math.floor(Math.random() * LIST_LENGTH);
+  const NAME = niceList[INDEX];
+
+  const proof = merkleTree.getProof(INDEX);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof,
+    leaf: NAME,
   });
 
   console.log({ gift });
